ca72ce5b5cf0a5cba11989b41ab812da79137756
julien-elodie <2644756656@qqicom>
2017-08-15 02:24:34 +0800

    version 1.0.0

--- weibo_video.py
+++ weibo_video.py
@@ -3,10 +3,11 @@
 
 from sina_login import Weibologin as login
 from bs4 import BeautifulSoup
-import re
 from urllib import unquote
 import os
 import requests
+import time
+import json
 
 login = login()
 login.simpleLogin()
@@ -23,6 +24,8 @@ class Videodownload(object):
         super(Videodownload, self).__init__()
         self.categories = {}
         self.videos = {}
+        self.end_id = 0
+        # self.totalNumber = 0
 
     def getCategries(self):
         url = 'http://weibo.com/tv'
@@ -41,30 +44,66 @@ class Videodownload(object):
                 if not os.path.exists(filedir):
                     os.mkdir(filedir)
 
-    def getVedios(self):
+    def getVideos(self):
         for item in self.categories.keys():
+            self.item = item
             url = self.categories[item]
-            res = login.session.get(url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            items = soup.body.div.find(class_='weibo_tv_frame')
-            videos = items.ul.find_all('a')
+            types = url.split('/')[-1]
+            pageNumber = 1
             videoNumber = 0
-            for video in videos:
-                videoNumber = videoNumber + 1
-                href = video.get('href')
-                name = video.li.find(class_='intra_a').find(
-                    class_='txt_cut').get_text()
-                url = 'http://weibo.com' + href
+            self.autoLoad(types, pageNumber, videoNumber)
+
+    def autoLoad(self, types, pageNumber, videoNumber):
+        while videoNumber <= 100:
+            if pageNumber == 1:
+                pageNumber = pageNumber + 1
+                url = 'http://weibo.com/tv/' + types
                 res = login.session.get(url)
                 soup = BeautifulSoup(res.text, 'html.parser')
-                items = soup.body.div.find(class_='weibo_player_wrap')
-                href = items.div.div.get('action-data').split('&')[5]
-                href = 'http:' + unquote(href.split('=')[1])
-                self.videos[url] = [name, href]
-                self.categories[item] = self.videos
-                filedir = dir + item + '/'
-                filename = name[0:17] + '.mp4'
-                self.videoDownload(href, filedir, filename)
+                items = soup.body.div.find(class_='weibo_tv_frame')
+                videos = items.ul.find_all('a')
+                for video in videos:
+                    videoNumber = videoNumber + 1
+                    href = video.get('href')
+                    name = video.li.find(class_='intra_a').find(
+                        class_='txt_cut').get_text()
+                    url = 'http://weibo.com' + href
+                    self.end_id = video.get('mid')
+                    self.getVideoUrl(url, name)
+            else:
+                end_id = self.end_id
+                __rnd = int(round(time.time()))
+                url = 'http://weibo.com/p/aj/v6/mblog/videolist?type=' + \
+                    types + '&page=' + \
+                    str(pageNumber) + '&end_id=' + \
+                    end_id + '&__rnd=' + str(__rnd)
+                pageNumber = pageNumber + 1
+                res = login.session.get(url)
+                text = json.loads(res.text)['data']['data']
+                soup = BeautifulSoup(text, 'html.parser')
+                videos = soup.find_all('a')
+                for video in videos:
+                    videoNumber = videoNumber + 1
+                    href = video.get('href')
+                    name = video.li.find(class_='intra_a').find(
+                        class_='txt_cut').get_text()
+                    url = 'http://weibo.com' + href
+                    self.end_id = video.get('mid')
+                    self.getVideoUrl(url, name)
+
+    def getVideoUrl(self, url, name):
+        res = login.session.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        items = soup.body.div.find(class_='weibo_player_wrap')
+        href = items.div.div.get('action-data').split('&')[5]
+        href = 'http:' + unquote(href.split('=')[1])
+        self.videos[url] = [name, href]
+        self.categories[self.item] = self.videos
+        filedir = dir + self.item + '/'
+        filename = name[0:17] + '.mp4'
+        self.videoDownload(href, filedir, filename)
+        # self.totalNumber = self.totalNumber+1
+        # print self.totalNumber
 
     def videoDownload(self, url, filedir, filename):
         print filename + u'开始下载'
@@ -85,10 +124,10 @@ class Videodownload(object):
                     print self.categories[item][lists][0]
                     print self.categories[item][lists][1]
 
-    def autoLoad(self):
-        // # TODO:
+    def simpleDownload(self):
+        self.getCategries()
+        self.getVideos()
 
 
 download = Videodownload()
-download.getCategries()
-download.getVedios()
+download.simpleDownload()
